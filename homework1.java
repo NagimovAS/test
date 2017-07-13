1ab3b5500aacb1b636ba1389d73a5c54e3c9f18e
Нагимов Азат <azat.nagimov@kidzania.ru>
2017-07-13 14:30:57 +0300

    my first homework

---
+++ homework1.java
@@ -0,0 +1,21 @@
+
+class homework1 {
+
+
+
+public static void main(String args[]){
+
+System.out.println( "\n summ is " + (Integer.valueOf(args[0])+Integer.valueOf(args[1])));
+
+System.out.println("\n your name is " + args[2] + args[3] + args[4] + args[5]);
+
+System.out.println("\n multiplicate is " + (Double.valueOf(args[6])*Double.valueOf(args[7])));
+
+System.out.println("\n divide is " + (Double.valueOf(args[8])/Double.valueOf(args[9])));
+
+System.out.println("\n Size of args[] is " + args.length);
+
+}
+
+
+}
